0aa01aa4f5bd657ab36acef519089a6673cedd74
Julius Ngwu <juliusngwu@Juliuss-MacBook-Pro.local>
2019-08-13 15:40:35 +0100

    chore(jest): setup test environment
    -create _test_ folder
    -install jest and enzymes with all other dependecies
    -create jest.config.js file
    -create tests.setup.js
    -add pull request template
    
    [Delivers #167790012]

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+// react libraries
+import React from 'react';
+
+/**
+ * @desc react compoment
+ * @return compoment App
+ */
+const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+  </div>
+);
+
+export default App;
