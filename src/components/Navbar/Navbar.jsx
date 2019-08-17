756046010641b29334985bb918c3716f04257e0d
Julius Ngwu <juliusngwu@Juliuss-MacBook-Pro.local>
2019-08-17 12:52:03 +0100

    feat(Homepage): user view home page
    -create home page component
    -create hpme scss
    
    [Delivers #167875805]

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../Home/home.scss';
+import '../../index.scss';
+
+
+class NavBar extends Component {
+
+  render() {
+    return (
+        <nav className="navbar">
+            <span className="navbar-toggle" id="js-navbar-toggle">
+                <i className="fa fa-bars"> </i>
+            </span>
+            <a href="index.html" className="logo"><b>B</b>anka</a>
+            <ul className="main-nav" id="js-menu">
+              <li>
+                  <NavLink to="./signup" className="nav-links">Sign Up</NavLink>
+              </li>
+              <li>
+                  <NavLink to="./login" className="nav-links">Login</NavLink>
+              </li>
+            </ul>
+        </nav>
+    );
+  }
+}
+
+export default NavBar;
